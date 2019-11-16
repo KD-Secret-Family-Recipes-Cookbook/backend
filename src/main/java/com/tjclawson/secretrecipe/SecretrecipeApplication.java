9efd87ca2e7876474e05f29a7a38f11ddaa4657b
tjclawson <tjamesclawson@gmail.com>
2019-11-16 15:16:57 -0500

    Initialize project

---
+++ src/main/java/com/tjclawson/secretrecipe/SecretrecipeApplication.java
@@ -0,0 +1,13 @@
+package com.tjclawson.secretrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretrecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecretrecipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tjclawson/secretrecipe/SecretrecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjclawson.secretrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretrecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
